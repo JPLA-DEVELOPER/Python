5b7126bb5455a9448e614ca5448e994361abbb5a
João Paulo  Lima de Albuquerque <jpla.developer@gmail.com>
2022-11-21 11:30:01 -0300

    tuplas

---
+++ Módulo10-Tuplas.py
@@ -0,0 +1,19 @@
+#----------------------TUPLAS --------------------------
+#---CARACTERÍSTICAS -----------
+#--Ordenados
+#--Permite valores duplicados
+#--São indexados
+#--Permite dados heterogêneos
+#--Elementos não-intercambiados(não dá para trocar elementos de lugar dentro de uma tupla)
+
+
+#--------------------------CRIAÇÃO------------------------------------
+tupla = (1, 2, 3)
+print(f'Tupla: {tupla}')
+
+#outra forma de criar tuplas:
+
+lista = [1,2,3]
+tupla2 = tuple(lista)
+print(f'Tupla2: {tupla2}')
+
