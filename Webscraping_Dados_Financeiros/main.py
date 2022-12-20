41fb3b8dd03b4b3f1117caf32016659895f661cb
João Paulo  Lima de Albuquerque <jpla.developer@gmail.com>
2022-12-20 15:58:00 -0300

    Projeto1

---
+++ Webscraping_Dados_Financeiros/main.py
@@ -0,0 +1,21 @@
+#Coleta de dados da web de forma automatizada.
+#CASOS DE USO: 
+#              Monitoração de preços.
+#              Monitoração de notícias.
+#              Geração de leads.
+#              Pesquisa de mercado.
+
+
+#Crawler: Navega na página web alvo  mapeando todas as páginas, caminhos, links, formulários, recursos para dowload.
+#Parsing: Análise sintática da página web, transformando o código HTML em uma representação mais fácil de ser processada.
+#Scraper: Extrai os dados da página através de seletores CSS, tags HTML, expressões regulares, XPath ou uma combinação deles. Necessita conhecimento prévio da estrutura da página. 
+#Existem métodos para dificultar esse processo(ofuscação: Técnica de embaralhar os dados da página, dificultando ou impossibilitando o processo de webscraping).
+
+#Pagina alvo: Fundamentus.
+
+#Ferramentas: Requests: Realiza a requisição HTTP.
+#             BeautifulSoup: Biblioteca que facilita o parsing de páginas HTML.
+#             Tabulate: Biblioteca para formatar dados no terminal.
+
+
+#
